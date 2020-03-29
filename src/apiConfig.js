eb781550ba77dd99a987b0044338557244788e2e
RadenAR <radenabdulrahman@gmail.com>
2020-03-29 15:51:47 -0400

    added url for back end

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://mysterious-tor-94072.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
